e26db714bbdc694880e757da6bc24bb95b32d6d5
Alex Vazquez <avazque008@outlook.com>
2022-06-03 15:36:02 -0400

    user-routes created

---
+++ controllers/api/chat-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Chat } = require('../../models');
+
+router.
+
+
+
+
+module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,12 +1,61 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const { Op } = require('sequelize');
 
-// login Route
+// SEARCH for users
+router.post('/search', (req, res) => {
+    User.findAll({
+        where: {
+            UsernameNormalized: {
+                [Op.like]: req.body.username.toUpperCase()
+            }
+        },
+        order: ["Username"]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this username' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
+// CREATE a user
+router.post('/', (req, res) => {
+    User.create({
+        Username: req.body.username,
+        UsernameNormalized: req.body.username.toUpperCase(),
+        Password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.Username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+// LogIn Route
 router.post('/login', (req, res) => {
     // expects {Username: 'lernantino@gmail.com', Password: 'password1234'}
     User.findOne({
         where: {
-            Username: req.body.Username
+            Username: req.body.username
         }
     }).then(dbUserData => {
         if (!dbUserData) {
