74b8c52b18cdbd66e8f7c958b06b8d7e91a24a0b
Alex Vazquez <avazque008@outlook.com>
2022-06-06 12:47:23 -0400

    made some changes to api routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const logInRoute = require('./login-route');
-const signUpRoute = require('./signup-route');
 
 router.use('/', logInRoute);
-router.use('/signup', signUpRoute);
 router.use('/api', apiRoutes);
 
 module.exports = router;
--- controllers/signup-route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-// render LogIn page
-router.get('/', (req, res) => {
-    res.render('signup-page');
-});
-
-module.exports = router;
