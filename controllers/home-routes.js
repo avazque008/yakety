b856a90714660881300b1c6497d3a7f24438bc62
Alex Vazquez <avazque008@outlook.com>
2022-06-07 18:40:32 -0400

    handlebars examples created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // render LogIn page
 router.get('/', (req, res) => {
-    res.render('login-page');
+    res.render('home-page');
 });
 
 module.exports = router;
---
+++ public/javascript/login.js
@@ -0,0 +1,57 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            // Testing
+            alert('You are now logged in!');
+            // 
+            document.location.replace('/home');
+
+        } else if (response.statusText === 'Bad Request') {
+            alert('Incorrect username or password, please try again!');
+        }
+    }
+}
+
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            //Testing
+            alert('You have signed up successfully!')
+            document.location.replace('/home');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
