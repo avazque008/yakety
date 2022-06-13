ba6542f8239e69b282cf1820c3de12d0c1c30d90
Alex Vazquez <avazque008@outlook.com>
2022-06-13 14:10:36 -0400

    fixed a error

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
