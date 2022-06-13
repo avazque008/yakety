8a79018661aaeecea65e7777153cdbde4706b03a
Alex Vazquez <avazque008@outlook.com>
2022-06-13 14:09:28 -0400

    connection file edited

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
-require('dotenv').config();
-
+// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql',
-    dialectOptions: {
-        decimalNumbers: true,
-    },
-});
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
